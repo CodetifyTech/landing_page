56350f1478e23149e9dbf191d7a4320af8ccf83c
Ngo Tran Hoang <hoangchomoi@gmail.com>
2024-05-19 22:39:27 +0700

    update

---
+++ kinh_doanh_quoc_te/assets/js/sendmail.js
@@ -0,0 +1,40 @@
+let form = document.getElementById("send_mail_form");
+let btnSubmit = form.querySelector("[type=submit]");
+const nganhIndex = 1;
+form.addEventListener("submit",  (e) => {
+    e.preventDefault();
+
+    let formData = new FormData(form);
+    sendData(formData.get("name"),formData.get("email"),formData.get("sdt"));
+}
+);
+
+const sendData = async (name,email,sdt) => {
+const formdata = new FormData();
+formdata.append("name", name);
+formdata.append("email", email);
+formdata.append("sdt", sdt);
+formdata.append("major", nganhIndex);
+
+const requestOptions = {
+    method: "POST",
+    body: formdata,
+};
+btnSubmit.innerHTML = "Đang gửi...";
+btnSubmit.disabled = true;
+fetch("https://xettuyen.fft.vn/api/landing_page/send-mail", requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+        alert("Gửi thành công");
+        console.log(result);
+        btnSubmit.innerHTML = "Gửi ngay";
+        btnSubmit.disabled = false;
+        form.reset();
+    })
+    .catch((error) => {
+        alert("Lỗi, thử lại sau");
+          btnSubmit.innerHTML = "Gửi ngay";
+          btnSubmit.disabled = false;
+        console.log(error);
+    });
+}
